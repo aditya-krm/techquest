9944fa8270929c0190e253a959c4fc4d6716f2be
Aditya Karmakar <adkarmakar521@gmail.com>
2024-10-06 00:09:43 +0530

    fixes bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,27 @@
 import Navbar from "@/components/customs/Nav";
 import "./globals.css";
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 import { Toaster } from "sonner";
 
-export const metaData = {
+export const metadata = {
   title: "TechQuest - a platform for tech enthusiasts",
-  description: "TechQuest is a platform for tech enthusiasts to learn, share, and grow together. We offer a range of resources, including blogs, quizzes, and interview preparation materials, to help you stay up-to-date with the latest trends in technology.",
-}
+  description:
+    "TechQuest is a platform for tech enthusiasts to learn, share, and grow together. We offer a range of resources, including blogs, quizzes, and interview preparation materials, to help you stay up-to-date with the latest trends in technology.",
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html>
-      <head>
-        <title>{metaData.title}</title>
-        <meta name="description" content={metaData.description} />
-      </head>
+      <head></head>
       <body>
         <main>
-        <Toaster position="top-right" richColors />
-          <Navbar/>
+          <Toaster position="top-right" richColors />
+          <Navbar />
           {children}
         </main>
       </body>
     </html>
   );
-}
+};
 
 export default RootLayout;
